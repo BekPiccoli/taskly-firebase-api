332aeac2f47b22e4bdc5d9140c88a04b9602f37f
Bernardo Piccoli <bernardokpiccoli@gmail.com>
2025-11-05 09:49:57 -0300

    feat: implementando rota de login

--- index.js
+++ index.js
@@ -3,32 +3,65 @@ import { db } from "./firebase.config.js";
 
 const port = 3001;
 const app = express();
-
-const hashPassword = async (password) => {
-  return btoa(password);
-};
 app.use(json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const hashPassword =  (password) => {
+  // return btoa(password);
+  return password;
+};
+
+const solvedingPassword =  (password) => {
+  // return atob(password);
+    return password;
+};
 
 app.post("/signup", async (req, res) => {
-  const { name, email, password } = req.body;
+  const { email, password } = req.body;
   const hashedPassword = await hashPassword(password);
   try {
     const userRef = db.collection("users").doc();
     await userRef.set({
-      name,
       email,
       password: hashedPassword,
     });
-    res.status(201).send({ id: userRef.id, name, email });
+    res.status(201).send({ id: userRef.id, email });
   } catch (error) {
     console.error("Error creating user:", error);
     res.status(500).send("Error creating user");
   }
 });
+
+app.get("/login", async (req, res) => {
+  try {
+    const {email, password} = req.body;
+    const solvedPassword = solvedingPassword(password);
+    const trimmedEmail = email.trim();
+    const usersRef = db.collection("users");
+    const snapshot = await usersRef.where("email", "==", trimmedEmail).get();
+
+    if (snapshot.empty) {
+      return res.status(404).send({ message: "Usuário não encontrado" });
+    }
+
+   let userData = null;
+    snapshot.forEach(doc => {
+      userData = {
+        id: doc.id,
+      ...doc.data(),
+        };
+});
+console.log(userData.password === solvedPassword)
+    if (userData.password === solvedPassword ) {
+     return res.send({Authenticated: true, user: userData})
+    } else{
+      return res.status(404).send({ message: "Usuário não encontrado" });
+    }
+
+  } catch(error) {
+    console.error(error)
+  }
+
+})
 
 app.get("/subjects", async (req, res) => {
   try {
