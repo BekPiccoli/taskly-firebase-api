83bc6ebd7d71fed042376f3f4d141b3b1a50e0c4
Bernardo Piccoli <bernardo@redrive.com.br>
2025-11-07 19:24:10 -0300

    asas

---
+++ controller/isAuthenticated.js
@@ -0,0 +1,32 @@
+export const isAuthenticated = async (req, res) => {
+  try {
+    const { email } = req.query;
+    const trimmedEmail = email.trim();
+    const usersRef = db.collection("users");
+    const snapshot = await usersRef
+      .where("email", "==", trimmedEmail)
+      .where("authenticated", "==", true)
+      .get();
+
+    if (snapshot.empty) {
+      return res.status(404).send({ message: "Usuário não autenticado" });
+    }
+
+    let userData = null;
+    snapshot.forEach((doc) => {
+      userData = {
+        id: doc.id,
+        ...doc.data(),
+      };
+    });
+
+    return res.status(200).send({
+      id: userData.id,
+      email: userData.email,
+      authenticated: userData.authenticated,
+    });
+  } catch (error) {
+    console.error("Error checking authentication:", error);
+    res.status(500).send("Error checking authentication");
+  }
+};
--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,10 @@ import { handleSignup } from "../controller/singup.js";
 import { handleLogout } from "../controller/logout.js";
 import { handleLogin } from "../controller/login.js";
 import { getSubjects } from "../controller/getSubjects.js";
-
 export const routes = (app) => {
-  app.get("/login", handleLogin);
   app.get("/subjects", getSubjects);
-
+  // app.get("/isAuthenticated", isAuthenticated);
+  app.post("/login", handleLogin);
   app.post("/signup", handleSignup);
   app.post("/logout", handleLogout);
 };
